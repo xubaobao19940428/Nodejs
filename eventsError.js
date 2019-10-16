bf903c5c852e8b3ddd150890e43b584ffbd4061d
xubaobao19940428 <915775317@qq.com>
2019-10-16 15:32:56 +0800

    buffer

--- eventsError.js
+++ eventsError.js
@@ -1,3 +1,10 @@
 //events的Error事件
+// error 事件
+// EventEmitter 定义了一个特殊的事件 error，它包含了错误的语义，我们在遇到 异常的时候通常会触发 error 事件。
+
+// 当 error 被触发时，EventEmitter 规定如果没有响 应的监听器，Node.js 会把它当作异常，退出程序并输出错误信息。
+
+// 我们一般要为会触发 error 事件的对象设置监听器，避免遇到错误后整个程序崩溃
 var EventsEmitter = require('events')
 var events = new EventsEmitter.EventEmitter()
+events.emit('error')
